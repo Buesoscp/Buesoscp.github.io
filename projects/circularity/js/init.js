c4a3e0f44763d00f66fc7511afda2745625ecbcd
Buesoscp <157516730+Buesoscp@users.noreply.github.com>
2024-05-07 16:18:12 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,17 +20,35 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;			// variable to hold a single circle when creating circles / iterating
+        var circles = [];	// variable to store all circles in one Array
 
         // TODO 2 : Create a function that draws a circle 
+          function drawCircle(){
+
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+          }
+          
         
-
+        
+        
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+          // do something
+         drawCircle();
+        }
+        
+         
+         
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
+              
         
         /* 
         This Function is called 60 times/second producing 60 frames/second.
@@ -39,11 +57,30 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+           
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i]);
+               }
+        
+            
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
-
+            game.checkCirclePosition(circle);
+            game.checkCirclePosition(circle);
+            game.checkCirclePosition(circle);
+            game.checkCirclePosition(circle);
+            game.checkCirclePosition(circle);
+        
+        
+            
+           
+           
+            }
+           
+           
+           
             // TODO 9 : Iterate over the array
            
             
@@ -62,7 +99,11 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            game.checkCirclePosition = function (circle) {
+                // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+                if (circle.x > canvas.width) {
+                  circle.x = 0;
+                }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
@@ -82,7 +123,7 @@ var init = function (window) {
         
         app.addUpdateable(window.opspark.game);
     }
-};
+    };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
